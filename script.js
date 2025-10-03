355c28aebcdf3951bd131fc7f7ea48b5f6d3e918
Hilton Eleuterio dos Santos Junior <110138764+HILTONESJR@users.noreply.github.com>
2025-10-03 11:16:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,8 @@ setInterval(function() {
       x.style.display = "block";
     }
 }
+
+window.onload = function() {
+        // Reset the form fields when the page loads
+        document.getElementById("form").reset();
+    };
